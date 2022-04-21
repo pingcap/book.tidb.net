65a8578fa653c8230af8435d5350a810e9b08bdc
sykp241095 <sykp241095@gmail.com>
2022-04-21 11:09:12 +0800

    Rename |

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "TiDB | Community",
-  tagline: "TiDB | Community",
+  title: "TiDB Books",
+  tagline: "TiDB Books",
   url: "https://tidb.net",
   baseUrl: "/book/",
   onBrokenLinks: "throw",
